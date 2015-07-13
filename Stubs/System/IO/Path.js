4e1db1289722c9554b07c203563a1e48fdb53386
SignpostMarv <me@signpostmarv.name>
2015-07-13 22:51:02 +0100

    adding System.IO.Path.GetDirectoryName() and System.IO.Path.GetFileName() methods to stub

--- Stubs/System/IO/Path.js
+++ Stubs/System/IO/Path.js
@@ -5,6 +5,19 @@ export default (function(){
     static Combine(){
       return Array.prototype.slice.call(arguments).join('/');
     }
+
+    static GetDirectoryName(path){
+      var
+        parts = path.split('/')
+      ;
+      parts.pop();
+
+      return parts.join('/');
+    }
+
+    static GetFileName(path){
+      return path.split('/').pop();
+    }
   }
 
   return Path;
