5e2d7c3c3144670efed85f98aacd1b7ded1bd0b3
SignpostMarv <me@signpostmarv.name>
2015-07-18 16:31:47 +0100

    using imports & a single function to indcate when FRONTIERS.js is ready

---
+++ examples/imports/onFrontiersReady.js
@@ -0,0 +1,23 @@
+System.transpiler = 'babel';
+window.onFrontiersReady = (function(){
+  'use strict';
+  var
+    loaded = false
+  ;
+  return function(cb, err){
+    if(loaded){
+      cb();
+      return;
+    }
+    err = err || function(msg){
+      console.error(msg);
+    };
+    System.import(
+      '../FRONTIERS.js'
+    ).then(function(Frontiers){
+      window.Frontiers = Frontiers.default;
+      loaded = true;
+      cb();
+    }, err);
+  };
+})();
